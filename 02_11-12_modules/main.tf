d7a323179d0fc885f8bf7badbdbded0207c9488a
Florento <florent.o@outlook.com>
2021-06-24 23:10:37 +0200

    to commit

--- 02_11-12_modules/main.tf
+++ 02_11-12_modules/main.tf
@@ -6,9 +6,8 @@ variable "aws_access_key" {}
 variable "aws_secret_key" {}
 
 variable "region" {
-  default = "us-east-2"
+  default = "eu-central-1"
 }
-
 
 # //////////////////////////////
 # PROVIDERS
@@ -64,7 +63,7 @@ module "vpc" {
   name = "frontend-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-east-2a", "us-east-2b", "us-east-2c"]
+  azs             = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
--- 03_02-03_remotestate/remote_resources/s3_backend.tf
+++ 03_02-03_remotestate/remote_resources/s3_backend.tf
@@ -6,7 +6,7 @@ variable "aws_access_key" {}
 variable "aws_secret_key" {}
 
 variable "bucket_name" {
-  default = "red30-tfstate"
+  default = "red30-tfstate-fo-2406"
 }
 
 # //////////////////////////////
@@ -15,7 +15,7 @@ variable "bucket_name" {
 provider "aws" {
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
-  region = "us-east-2"
+  region = "eu-central-1"
 }
 
 # //////////////////////////////
